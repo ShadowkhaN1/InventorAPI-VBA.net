11201f81950e5159e6e0a194bf96c2ba7ff80fb5
ShadowkhaN1 <lukenu000@utpedu.pl>
2019-09-01 09:30:12 +0200

    first commit

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports Inventor
+Imports System.Runtime.InteropServices
+
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inventorApp As Inventor.Application
+
+        Try
+            inventorApp = Marshal.GetActiveObject("Inventor.Application")
+            MessageBox.Show("Connect with Inventor")
+
+        Catch ex As Exception
+            MessageBox.Show("Cannot connect to Inventor")
+        End Try
+
+        Dim oPartDoc As PartDocument
+
+        oPartDoc = inventorApp.ActiveDocument
+
+        oPartDoc.ComponentDefinition.Parameters.Item("d0").Value = Convert.ToInt32(TextBox1.Text)
+        oPartDoc.ComponentDefinition.Parameters.Item("d1").Value = Convert.ToInt32(TextBox2.Text)
+        oPartDoc.ComponentDefinition.Parameters.Item("d2").Value = Convert.ToInt32(TextBox3.Text)
+
+        oPartDoc.Update()
+
+    End Sub
+End Class
